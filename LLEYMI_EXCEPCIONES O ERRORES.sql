61470c790bc41b3564d020819881f0bb3ffeae6d
Lleymi Cruz <71413709+LleymiCruz@users.noreply.github.com>
2022-10-14 19:12:23 -0600

    Update LLEYMI_EXCEPCIONES O ERRORES.sql

--- LLEYMI_EXCEPCIONES O ERRORES.sql
+++ LLEYMI_EXCEPCIONES O ERRORES.sql
@@ -29,3 +29,38 @@ WHEN too_many_rows THEN
     DBMS_OUTPUT.PUT_LINE('mas de una fila');
 end;
 /
+DECLARE
+   reg_max EXCEPTION;
+   regn NUMBER;
+   regt varchar2(200);
+BEGIN
+   regn:=101;
+   regt:='ASIA';
+   IF regn > 100 THEN
+         RAISE reg_max;  
+    ELSE
+       insert into regions values (regn,regt);
+       commit;
+      END IF;
+EXCEPTION
+  WHEN reg_max THEN  
+    DBMS_OUTPUT.PUT_LINE('La region no puede ser mayor de 100.');
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error indefinido');
+END;
+/
+
+DECLARE   
+   regn NUMBER;
+   regt varchar2(200);
+BEGIN
+   regn:=101;
+   regt:='ASIA';
+   iF regn > 100 THEN
+       -- EL CODIGO DEBE ESTAR ENTRE -20000 Y -20999
+       RAISE_APPLICATION_ERROR(-20001,'LA ID NO PUEDE SER MAYOR DE 100');  
+    ELSE
+       insert into regions values (regn,regt);
+       commit;
+    END IF;
+END;
